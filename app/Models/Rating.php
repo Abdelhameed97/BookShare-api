d83cff6ba2fb8ccac087f148dbce0a246ed0d7ae
Abdelhameed Mohammed <abdelhameed7mohammed21@gmail.com>
2025-05-28 01:12:46 +0300

    add relations for ratings and wishlist.

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -9,4 +9,27 @@ class Rating extends Model
 {
     /** @use HasFactory<\Database\Factories\RatingFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'book_id',
+        'reviewer_id',
+        'reviewed_user_id',
+        'rating',
+        'comment',
+    ];
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function reviewer()
+    {
+        return $this->belongsTo(User::class, 'reviewer_id');
+    }
+
+    public function reviewedUser()
+    {
+        return $this->belongsTo(User::class, 'reviewed_user_id');
+    }
 }
