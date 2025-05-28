6281dbcdd744daa84e57a9a4fb9254dc2e48a561
i <harbia305@gmail.com>
2025-05-28 15:08:00 +0300

    Commit changes before merging

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CommentController;
+use App\Http\Controllers\API\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('comments', CommentController::class);
+
+Route::resource('categories', CategoryController::class);
+
