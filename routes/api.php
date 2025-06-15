072986c082eb07c3d061b26f96bf1433c5078d9e
harbi <harbia305@gmail.com>
2025-06-15 22:18:04 +0300

    EDIT CART AND UPDTED

--- routes/api.php
+++ routes/api.php
@@ -93,5 +93,6 @@ With best regards, team BookShare', function ($message) {
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/cart', [CartController::class, 'index']);
     Route::post('/cart', [CartController::class, 'store']);
-    Route::delete('/cart/{id}', [CartController::class, 'destroy']);
+    Route::put('/cart/{id}', [CartController::class, 'update']);
+    Route::delete('/cart/{id}', [CartController::class, 'destroy']); 
 });
