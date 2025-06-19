f4638df315e99598720c8ae12ea4631e9ecd3111
Abdelhameed Mohammed <abdelhameed7mohammed21@gmail.com>
2025-06-19 04:41:05 +0300

    solve some fields datatype to make it suitable for front-end.

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
+use App\Models\Client;
+use App\Models\Owner;
 
 class AuthController extends Controller
 {
@@ -28,6 +30,12 @@ class AuthController extends Controller
             'location'=>$validated['location']??null,
 
         ]);
+         // Create related record based on role
+        if ($user->role === 'client') {
+            Client::create(['user_id' => $user->id]);
+        } elseif ($user->role === 'owner') {
+            Owner::create(['user_id' => $user->id]);
+        }
 
         $token = $user->createToken('auth_token')->plainTextToken;
         return response()->json([
@@ -50,23 +58,21 @@ class AuthController extends Controller
         $user = User::where('email',$request->email)->first();
 
         // check if user exists and password is correct
-        if(!$user && !Hash::check($request->password, $user->password)){
+        if(!$user || !Hash::check($request->password, $user->password)){
             return response()->json([
                 'message'=>'Invalid login details',
             ],401);
         }
 
         // check if user has any tokens
-        $tokens = $user->tokens()->count();
 
-        if($tokens > 2){
-            return response()->json([
-                'message' => 'You have exceeded the maximum number of login attempts.'
-            ], 401);
-        }else{
-            // create token
-            $token = $user->createToken('auth_token')->plainTextToken;
+        $maxTokens = 5;
+
+        if ($user->tokens()->count() >= $maxTokens) {
+            $user->tokens()->oldest()->first()->delete(); // delete oldest token
         }
+        $token = $user->createToken('auth_token')->plainTextToken;
+
 
         // return response
         return response()->json([
