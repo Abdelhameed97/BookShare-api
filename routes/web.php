21eff5b32ed20a90dd286d9bebaa28a57337d121
Abdelhameed Mohammed <abdelhameed7mohammed21@gmail.com>
2025-06-28 22:48:30 +0300

    forget-password udate.

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SocialAuthController;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
+use Illuminate\Http\Request;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/auth/{provider}/redirect', [SocialAuthController::class, 'redirectToProvider']);
 Route::get('/auth/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback']);
 
 
-Route::get('/reset-password/{token}', function ($token) {
-    return redirect(env('FRONTEND_URL') . "/reset-password?token=$token");
+Route::get('/reset-password/{token}', function (Request $request, $token) {
+    $email = $request->query('email');
+    return redirect(env('FRONTEND_URL') . "/reset-password?token=$token&email=$email");
 })->name('password.reset');
 
